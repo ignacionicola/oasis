516a9a299ab9abafad8bf41ab103d1a0d1d52e87
Ignacio <ignaciopvp1212@gmail.com>
2026-05-17 21:59:54 -0300

    solucuinando expo

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,5 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // react-native-worklets/plugin debe ir SIEMPRE último.
+    // Requerido por react-native-reanimated v4.
+    plugins: ['react-native-worklets/plugin'],
   };
 };
